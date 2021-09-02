080368fd7fd26e7c3f649b4bcd571f2385c89ab8
Jan Sorgalla <jsorgalla@gmail.com>
2021-09-02 13:52:03 +0200

    Remove json_last_error_msg() polyfill

--- src/Internal/HalResourceFactory.php
+++ src/Internal/HalResourceFactory.php
@@ -146,7 +146,7 @@ final class HalResourceFactory
             throw new Exception\BadResponseException(
                 sprintf(
                     'JSON parse error: %s.',
-                    self::getLastJsonError()
+                    json_last_error_msg()
                 ),
                 $request,
                 $response,
@@ -155,27 +155,5 @@ final class HalResourceFactory
         }
 
         return $data;
-    }
-
-    private static function getLastJsonError()
-    {
-        if (function_exists('json_last_error_msg')) {
-            return json_last_error_msg();
-        }
-
-        static $errors = [
-            JSON_ERROR_NONE           => null,
-            JSON_ERROR_DEPTH          => 'Maximum stack depth exceeded',
-            JSON_ERROR_STATE_MISMATCH => 'Underflow or the modes mismatch',
-            JSON_ERROR_CTRL_CHAR      => 'Unexpected control character found',
-            JSON_ERROR_SYNTAX         => 'Syntax error, malformed JSON',
-            JSON_ERROR_UTF8           => 'Malformed UTF-8 characters, possibly incorrectly encoded',
-        ];
-
-        $error = json_last_error();
-
-        return array_key_exists($error, $errors)
-            ? $errors[$error]
-            : sprintf('Unknown error (%s)', $error);
     }
 }
